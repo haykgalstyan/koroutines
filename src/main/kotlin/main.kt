0f202a8dec38d56bfadc81e55ac191211a8a682c
Hayk Galstyan <hgalstyan@universe.dart.spb@hgalstyan.nb.ipa.dataart.net>
2021-06-15 15:31:30 +0400

    added article link

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,9 @@
  * Coroutine examples
  * Read more here:
  * https://kotlinlang.org/docs/coroutines-guide.html
+ *
+ * Other useful articles:
+ * https://www.infoq.com/articles/kotlin-coroutines-bottom-up/
  */
 
 fun main() {
